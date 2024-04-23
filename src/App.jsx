4c0ddbc91113d853a29086a5c1c0b3ef0fcb255f
hessamsalehi <hessam.salehi1992@gmail.com>
2024-04-23 17:22:25 +0330

    Setting up styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/gloabalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-`;
-
-const Input = styled.input`
-  padding: 0.8rem 1rem;
-  border: 1px solid #ddd;
-  border-radius: 5px;
+const StyledApp = styled.main`
+  background-color: red;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>The Wild OASIS</H1>
-      <Button>Check in</Button>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild OASIS</Heading>
+        <Button>Check in</Button>
+        <Button>Check Out</Button>
+        <Input placeholder="Enter data" type="number" />
+      </StyledApp>
+    </>
   );
 }
 
