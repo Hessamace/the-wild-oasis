c0eede6d85f53fbcd63305c8bef99a60377ba58d
hessamace <hessam.salehi1992@gmail.com>
2024-05-04 13:33:01 +0330

    Added React Query to Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GloabalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -11,9 +19,16 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    staleTime: 60 * 1000,
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +45,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
