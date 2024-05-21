d819cfcece34a009fdcbab495c8cdeb12978075d
hessamace <hessam.salehi1992@gmail.com>
2024-05-21 23:27:46 +0330

    Added ProtectedRoute requirements

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking.jsx";
 import Checkin from "./pages/Checkin.jsx";
+import ProtectedRoute from "./ui/ProtectedRoute.jsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,7 +36,7 @@ function App() {
       <GlobalStyles/>
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout/>}>
+          <Route element={<ProtectedRoute><AppLayout/></ProtectedRoute>}>
             <Route index element={<Navigate replace to="dashboard"/>}/>
             <Route path="dashboard" element={<Dashboard/>}/>
             <Route path="bookings" element={<Bookings/>}/>
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,16 @@
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {useNavigate} from "react-router-dom";
 import {login as loginApi} from "../../services/apiAuth.js";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const {mutate: login, isLoading} = useMutation({
     mutationFn: ({email, password}) => loginApi({email, password}),
-    onSuccess: (data) => {
-      console.log(data);
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user);
       navigate("/dashboard");
     },
     onError: error => {
