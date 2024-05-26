0aab23bd91255534e9b7f23d8883e8f8a93f3bdb
hessamsalehi <hessam.salehi1992@gmail.com>
2024-05-26 09:51:02 +0330

    Added Stays for current day

--- src/features/check-in-out/TodayActivity.jsx
+++ src/features/check-in-out/TodayActivity.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
+import {useTodayActivity} from "./useTodayActivity.js";
+import Spinner from "../../ui/Spinner.jsx";
+import TodayItem from "./TodayItem.jsx";
 
 const StyledToday = styled.div`
   /* Box */
@@ -36,14 +39,25 @@ const NoActivity = styled.p`
   margin-top: 0.8rem;
 `;
 
-function Today() {
+function TodayActivity() {
+  const {isLoading , activities} = useTodayActivity()
+
   return (
     <StyledToday>
       <Row type="horizontal">
         <Heading as="h2">Today</Heading>
       </Row>
+      {
+        isLoading
+          ? <Spinner />
+          : activities?.length > 0
+              ? <TodayList>
+                  {activities.map(item => <TodayItem key={item.id} activity={item} />)}
+                </TodayList>
+              : <NoActivity />
+      }
     </StyledToday>
   );
 }
 
-export default Today;
+export default TodayActivity;
